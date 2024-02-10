849c4ab64a4a6e65821cfd74a56f8a29d4c67204
TheWisePigeon <zozozozeph@gmail.com>
2024-02-10 22:59:37 +0000

    format code

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -9,26 +9,26 @@ export default function RegistrationPage() {
   const [password, setPassword] = useState("")
 
   async function submit() {
-    try{
-      if(country === "" || city === "" || description ==="" || password ===""){
+    try {
+      if (country === "" || city === "" || description === "" || password === "") {
         alert("Veillez remplir tous les champs")
-      }else{
+      } else {
         const body = {
-          pays:country,
-          ville:city,
-          description:description,
-          mdp:password
+          pays: country,
+          ville: city,
+          description: description,
+          mdp: password
         }
 
         const response = await fetch(
           "API_URL",
           {
-            method:"POST",
-            body:JSON.stringify(body)
+            method: "POST",
+            body: JSON.stringify(body)
           }
         )
 
-        switch(response.status){
+        switch (response.status) {
           case 500:
             console.log("There might be an error")
             return
@@ -36,7 +36,7 @@ export default function RegistrationPage() {
             console.log("Registration complete")
         }
       }
-    }catch(error){
+    } catch (error) {
       console.log(error)
     }
   }
@@ -49,10 +49,10 @@ export default function RegistrationPage() {
             <label className="font-poppins_semiBold text-[24px]">COMPLETER VOS INFOS</label>
           </div>
           <div className="flex flex-col gap-[20px]">
-            <input name="countryInput" onChange={(e) => {setCountry(e.target.value)} } type="text" className="border border-secondary outline-secondary shadow h-[42px] w-[420px] rounded-[8px] p-[14px]" placeholder="Indiquez votre pays" />
-            <input name="cityInput" onChange={(e) => {setCity(e.target.value)} } type="text" className="border border-secondary outline-secondary shadow h-[42px] w-[420px] rounded-[8px] p-[14px]" placeholder="Indiquez votre ville" />
-            <textarea name="descriptionText" onChange={(e) => {setDescription(e.target.value)} } rows={10} className="border border-secondary outline-secondary shadow min-h-[100px] h-[42px] w-[420px] rounded-[8px] p-[14px] resize-none" placeholder="Donnez une brève description de vos produits" />
-            <input name="passwordInput" onChange={(e) => {setPassword(e.target.value)} } type="password" className="border border-secondary outline-secondary shadow h-[42px] w-[420px] rounded-[8px] p-[14px]" placeholder="Votre mot de passe" />
+            <input name="countryInput" onChange={(e) => { setCountry(e.target.value) }} type="text" className="border border-secondary outline-secondary shadow h-[42px] w-[420px] rounded-[8px] p-[14px]" placeholder="Indiquez votre pays" />
+            <input name="cityInput" onChange={(e) => { setCity(e.target.value) }} type="text" className="border border-secondary outline-secondary shadow h-[42px] w-[420px] rounded-[8px] p-[14px]" placeholder="Indiquez votre ville" />
+            <textarea name="descriptionText" onChange={(e) => { setDescription(e.target.value) }} rows={10} className="border border-secondary outline-secondary shadow min-h-[100px] h-[42px] w-[420px] rounded-[8px] p-[14px] resize-none" placeholder="Donnez une brève description de vos produits" />
+            <input name="passwordInput" onChange={(e) => { setPassword(e.target.value) }} type="password" className="border border-secondary outline-secondary shadow h-[42px] w-[420px] rounded-[8px] p-[14px]" placeholder="Votre mot de passe" />
             <button type="submit" className="bg-primary shadow h-[42px] w-[420px] rounded-[8px] text-center text-white text-[16px] mt-6">Complèter votre inscription</button>
           </div>
         </div>
