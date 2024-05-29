969ecc243a31fbc261724bc6223c79242d03e723
richotaku <otakuagbolossou@gmail.com>
2024-05-29 20:13:44 +0000

    design page

--- src/components/AddCategory.tsx
+++ src/components/AddCategory.tsx
@@ -1,15 +1,19 @@
+import { FaArrowLeft } from "react-icons/fa6";
 function AddCategory() {
   return (
     <>
       <div className="bg-[#F1F4F6] px-10 py-5">
-        <h2 className="flex">Ajouter catégorie</h2>
+        <div className="flex items-center gap-4">
+          <FaArrowLeft />
+          <h2>Ajouter catégorie</h2>
+        </div>
       </div>
-      <div className="flex flex-col gap-5 items-center">
-        <div className="flex flex-col gap-1">
+      <div className="flex flex-col gap-5 items-center mt-[12rem]  ">
+        <div className="flex flex-col gap-2">
           <h2>nom catégorie</h2>
           <input type="text" />
         </div>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-2">
           <h2>Description</h2>
           <input type="text" />
         </div>
